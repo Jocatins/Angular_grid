4e86cd59feaa55f6531263eafe5e1bf2b4e83ccb
Jocatins <jocakhamen@gmail.com>
2020-10-09 03:21:42 +0100

    Angular grid 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { PageSettingsModel } from '@syncfusion/ej2-angular-grids';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<ejs-grid
+    [allowSorting]="true"
+    [allowGrouping]="true"
+    [allowFiltering]="true"
+    [dataSource]="data"
+    [allowPaging]="true"
+    [pageSettings]="pageSettings"
+  >
+    <e-columns>
+      <e-column field="ProdId" headerText="ORDER ID"></e-column>
+      <e-column field="CompanyName" headerText="Company Name"></e-column>
+      <e-column field="Products" headerText="Products"></e-column>
+    </e-columns>
+  </ejs-grid>`,
+  // templateUrl: './app.component.html'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angrid';
+  public pageSettings: PageSettingsModel = { pageSize: 2 };
+  public data: object[] = [
+    {
+      ProdId: 1,
+      CompanyName: 'Total',
+      Products: 'Oil',
+    },
+    {
+      ProdId: 2,
+      CompanyName: 'Shell',
+      Products: 'Oil',
+    },
+    {
+      ProdId: 3,
+      CompanyName: 'TeqBridge',
+      Products: 'Software',
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  GridModule,
+  PagerModule,
+  PageService,
+  SortService,
+  FilterService,
+  GroupService,
+} from '@syncfusion/ej2-angular-grids';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, GridModule, PagerModule],
+  providers: [PageService, SortService, FilterService, GroupService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
